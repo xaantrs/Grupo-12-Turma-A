01019d8dfc9631682e9018bcb22a698e1557ae29
Vinicius <vinicius.bessa@pwi.com.br>
2023-11-10 14:24:15 -0300

    criando funcionalidade de instrucoes e créditos.

--- untitled/src/RPG/rpg.java
+++ untitled/src/RPG/rpg.java
@@ -2,75 +2,66 @@ package RPG;
 import java.util.Scanner;
 
 public class rpg {
-
+    static Scanner entrada = new Scanner(System.in);
     public static void main(String[] args) {
-        Scanner entrada = new Scanner(System.in);
 
-        //Nome do jogo
-        estiloEscrita("\n↞Contos de Horror da Mimi↠\n");
+        int contador = 0;
 
-        //Mensagem de Bem vindo com nome de usuário da máquina
-        String nomeUsu = pegaNome();
-        estiloEscrita("\nBem vindo, " + nomeUsu +  ".\nEscolha o que deseja fazer:\n" + "\n1. Instruções \n2. Jogar \n3. Créditos \n4. Sair\n");
+        do{
+            estiloEscrita("\n↞Contos de Horror da Mimi↠\n");
 
-        int acaoInicio = entrada.nextInt();
+            String nomeUsu = pegaNome();
+            estiloEscrita("\nBem vindo, " + nomeUsu +  ".\nEscolha o que deseja fazer:\n" + "\n1. Instruções \n2. Jogar \n3. Créditos \n4. Sair\n");
 
-        //criar uma função pra cada escolha pra facilitar a leitura do código
-        if (acaoInicio == 1) {
-            //instrucoes - fazer instruções no final já que não sabemos como o código e as questões funcionarão ainda
-            //lembrar de colocar aviso para deixar o jogo em tela cheia pra melhorar a experiencia.
-            instrucoes();
+            int acaoInicio = entrada.nextInt();
 
-        } else if (acaoInicio == 2){
-            //jogar
-            System.out.println("Jogar");
+            if (acaoInicio == 1) {
+                contador = instrucoes();
 
-        } else if (acaoInicio == 3){
-            //créditos
-            System.out.println("Creditos");
+            } else if (acaoInicio == 2){
+                System.out.println("Jogar");
 
-        } else if (acaoInicio == 4){
-            //sair
-            System.out.println("Sair");
+            } else if (acaoInicio == 3){
+                contador = creditos();
 
-        } else {
-            System.out.println("Opção inválida.");
-        }
+            } else if (acaoInicio == 4){
+                System.out.println("Sair");
+
+            } else {
+                System.out.println("Opção inválida.");
+            }
+
+        } while( contador == 1);
 
         entrada.close();
-
     }
 
-    //funções de funcionamento do jogo
-    //Nas opções instrucoes e creditos, colocar opção de voltar para o menu
-    static void instrucoes(){
+    static int instrucoes(){
         String usuario = pegaNome();
         estiloEscrita("\nInstruções: \n1 - Fique vivo \n2 - Não olhe para trás, " + usuario + ".\n\nDeseja retornar para o menu? \n1. Sim | 2. Não \n");
-        int saidaIntrucoes = 1;
 
-        if(saidaIntrucoes == 1){
-            System.out.println("ainda nao fiz ele voltar :)");
-        }else{
-
-        }
+        int voltaMenu = entrada.nextInt();
+        return voltaMenu;
     }
 
     static void jogar(){
 
     }
-    static void creditos(){
+    static int creditos(){
+        estiloEscrita("Creditos\n");
 
+        int voltaMenu = entrada.nextInt();
+        return voltaMenu;
     }
     static void sair(){
 
     }
 
-
     static void estiloEscrita(String texto) {
         for (char caractere : texto.toCharArray()) {
             System.out.print(caractere);
             try {
-                Thread.sleep(100);
+                Thread.sleep(1);
             }catch(InterruptedException e) {
                 e.printStackTrace();
 
